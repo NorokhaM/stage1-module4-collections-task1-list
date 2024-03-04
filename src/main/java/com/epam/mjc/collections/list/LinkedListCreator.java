1596045e2183c02c6bfdb57ed715096bf4b5d1bb
Maksym Norokha <maxnorohich@gmail.com>
2024-03-04 14:44:24 +0100

    123

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (Integer a : sourceList) {
+            if (a%2==0){
+                linkedList.addLast(a);
+            }
+            else{
+                linkedList.addFirst(a);
+            }
+        }
+        return linkedList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,18 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int intA = Integer.parseInt(a);
+        int intB = Integer.parseInt(b);
+        int valueA = 5 * intA * intA + 3;
+        int valueB = 5 * intB * intB + 3;
+        int compared = valueA - valueB;
+        return  (compared) == 0 ? intA - intB : compared;
     }
 }
